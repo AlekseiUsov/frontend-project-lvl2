043e464bd02b092521bfa8df30a56b897db52982
AlekseiUsov <aleksei.usoff@gmail.com>
2022-01-26 20:09:35 +0300

    Step 3

--- src/gendiff.js
+++ src/gendiff.js
@@ -19,22 +19,18 @@ const genDiff = (filepath1, filepath2) => {
 
     const lines = _.sortBy(Object.entries({ ...file2, ...file1 }))
         .reduce((acc, [key, value]) => {
-            if (_.has(file1, key) && _.has(file2, key)) {
-                if (file1[key] === file2[key]) {
-                    acc[`  ${key}`] = value;
-                } else if (file1[key] !== file2[key]) {
-                    acc[`${operators.minus} ${key}`] = file1[key];
-                    acc[`${operators.plus} ${key}`] = file2[key];
-                }
-            } else {
-                if (_.has(file1, key)) {
-                    acc[`${operators.minus} ${key}`] = value;
-                } else if (_.has(file2, key)) {
-                    acc[`${operators.plus} ${key}`] = value;
-                }
+            if (_.has(file1, key) && _.has(file2, key) && file1[key] === file2[key]) {
+                acc[`  ${key}`] = value;
+            } else if (_.has(file1, key) && _.has(file2, key) && file1[key] !== file2[key]) {
+                acc[`${operators.minus} ${key}`] = file1[key];
+                acc[`${operators.plus} ${key}`] = file2[key];
+            } else if ((_.has(file1, key)) && (!_.has(file2, key))) {
+                acc[`${operators.minus} ${key}`] = value;
+            } else if ((!_.has(file1, key)) && (_.has(file2, key))) {
+                acc[`${operators.plus} ${key}`] = value;
             }
             return acc;
-        }, {});
+        }, {})
     const result = JSON.stringify(lines, null, 2);
     return result.replace(/"/g, '').replace(/,/g, '');
 }
