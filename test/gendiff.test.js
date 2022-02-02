1faef13d13b827b088bdbbbd18c6ff76314fb158
AlekseiUsov <aleksei.usoff@gmail.com>
2022-02-02 14:58:32 +0300

    Step 4

--- test/gendiff.test.js
+++ test/gendiff.test.js
@@ -7,10 +7,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-console.log(genDiff('file1.json', 'file2.json'))
 
 test('check diff', () => {
-    const actual = genDiff('file1.json', 'file2.json');
+    const file1Path = getFixturePath('file1.json');
+    const file2Path = getFixturePath('file2.json');
+
+    const actual = genDiff(file1Path, file2Path);
     const plain = readFile('plain.txt');
     expect(actual).toEqual(plain);
 });
